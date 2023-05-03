7b3f9f7cbde323c750761cafaeb4238710e79f8b
Jurre Stender <jurre@github.com>
2023-05-03 18:51:20 +0200

    Add repo-private attribute to job
    This is a new attribute that we can use to improve our observability
    internally at GitHub.

--- internal/model/job_test.go
+++ internal/model/job_test.go
@@ -92,7 +92,7 @@ job:
     provider: github
     repo: dependabot/dependabot-core
     directory: "/npm_and_yarn/helpers"
-    branch: 
+    branch:
     api-endpoint: https://api.github.com/
     hostname: github.com
   dependencies:
@@ -323,21 +323,21 @@ job:
   update-subdependencies: false
   ignore-conditions:
   - dependency-name: npm
-    version-requirement: 
+    version-requirement:
     update-types:
     - version-update:semver-major
     source: ".github/dependabot.yml"
   - dependency-name: npm
     version-requirement: ">= 7.a, < 8"
-    update-types: 
+    update-types:
     source: "@dependabot ignore command"
-  requirements-update-strategy: 
+  requirements-update-strategy:
   allowed-updates:
   - dependency-type: direct
     update-type: all
   dependency-groups:
   - name: npm
-    rules: 
+    rules:
       patterns: ["npm", "@npmcli*"]
   credentials-metadata:
   - type: git_source
@@ -357,8 +357,9 @@ job:
     npm-transitive-security-updates: true
   reject-external-code: false
   commit-message-options:
-    prefix: 
-    prefix-development: 
-    include-scope: 
+    prefix:
+    prefix-development:
+    include-scope:
   security-updates-only: true
+  repo-private: false
 `
