12f9c3a95949a03550d4c16ad097b3aad0332a54
Jeff Widman <jeff@jeffwidman.com>
2023-07-11 11:48:38 -0700

    Remove smoke-related scripts (#144)
    These now live in the smoke tests repo:
    https://github.com/dependabot/smoke-tests/tree/cd5c5b5d8fb9abbd0f6db98c347dd9f7d065c6d4/script
    
    There's no reason for them to continue to have a copy here in the `cli`
    repo. They've only been touched once, on initial commit.
    
    1. The `run-all.sh` script is now outdated, as it doesn't list the most
       recent tests we've added. And it's not even called anywhere in this
       repo, so only useful for local development, where we'd just pull down
       the latest from the smoke test repo anyway.
    2. The `download-cache.sh` file is super simple, and in the other
       consumers of the smoke tests we've simply inlined it, for example: https://github.com/dependabot/dependabot-core/blob/6e39f38df76e941bc84541a806d6c82ff4f2cbf2/.github/workflows/smoke.yml#L244-L250
       So inline it here as well.

--- script/run-all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script is useful for regenerating all of the smoke tests running locally.
-
-declare -a arr=("actions" "bundler" "cargo" "composer" "docker" "elm" "go" "gradle" "hex" "maven" "npm" "nuget" "pip" "pip-compile" "pipenv" "poetry" "pub" "submodules" "terraform")
-for eco in "${arr[@]}"
-do
-  go run cmd/dependabot/dependabot.go test -f "testdata/smoke-$eco.yaml" -o "testdata/smoke-$eco.yaml"
-done
