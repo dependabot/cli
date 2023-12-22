e4b32bdea7c5cfc652dee6b75438b571bff9f10a
Jake Coffman <jake@jakecoffman.com>
2023-12-22 12:06:53 -0600

    this test no longer needed

--- internal/infra/run_test.go
+++ internal/infra/run_test.go
@@ -1,10 +1,7 @@
 package infra
 
 import (
-	"archive/tar"
-	"bytes"
 	"context"
-	"io"
 	"net/http"
 	"os"
 	"reflect"
@@ -13,11 +10,7 @@ import (
 
 	"github.com/dependabot/cli/internal/server"
 
-	"gopkg.in/yaml.v3"
-
 	"github.com/dependabot/cli/internal/model"
-	"github.com/docker/docker/api/types"
-	"github.com/moby/moby/client"
 )
 
 func Test_checkCredAccess(t *testing.T) {
@@ -166,169 +159,3 @@ func Test_generateIgnoreConditions(t *testing.T) {
 		}
 	})
 }
-
-func TestRun(t *testing.T) {
-	if testing.Short() {
-		t.Skip("skipping integration test")
-	}
-
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	ctx := context.Background()
-
-	var buildContext bytes.Buffer
-	tw := tar.NewWriter(&buildContext)
-	_ = addFileToArchive(tw, "/Dockerfile", 0644, dockerFile)
-	_ = addFileToArchive(tw, "/test_main.go", 0644, testMain)
-	_ = tw.Close()
-
-	UpdaterImageName := "test-updater"
-	resp, err := cli.ImageBuild(ctx, &buildContext, types.ImageBuildOptions{Tags: []string{UpdaterImageName}})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	_, _ = io.Copy(io.Discard, resp.Body)
-	_ = resp.Body.Close()
-
-	defer func() {
-		_, _ = cli.ImageRemove(ctx, UpdaterImageName, types.ImageRemoveOptions{})
-	}()
-
-	cred := model.Credential{
-		"type":     "git_source",
-		"host":     "github.com",
-		"username": "x-access-token",
-		"password": "$LOCAL_GITHUB_ACCESS_TOKEN",
-	}
-
-	os.Setenv("LOCAL_GITHUB_ACCESS_TOKEN", "test-token")
-	err = Run(RunParams{
-		PullImages: true,
-		Job: &model.Job{
-			PackageManager: "ecosystem",
-			Source: model.Source{
-				Repo: "org/name",
-			},
-		},
-		Creds:        []model.Credential{cred},
-		UpdaterImage: UpdaterImageName,
-		Output:       "out.yaml",
-	})
-	if err != nil {
-		t.Error(err)
-	}
-
-	f, err := os.Open("out.yaml")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer f.Close()
-
-	var output model.Scenario
-	if err = yaml.NewDecoder(f).Decode(&output); err != nil {
-		t.Fatal(err)
-	}
-
-	if !reflect.DeepEqual(output.Input.Credentials, []model.Credential{cred}) {
-		t.Error("unexpected credentials", output.Input.Credentials)
-	}
-	if output.Input.Credentials[0]["password"] != "$LOCAL_GITHUB_ACCESS_TOKEN" {
-		t.Error("expected password to be masked")
-	}
-}
-
-const dockerFile = `
-FROM golang:1.21
-
-# needed to run update-ca-certificates
-RUN apt-get update && apt-get install -y ca-certificates
-# cli will try to start as dependabot
-RUN useradd -ms /bin/bash dependabot
-
-# need to be the user for permissions to work
-USER dependabot
-WORKDIR /home/dependabot
-COPY *.go .
-# cli runs bin/run (twice) to do an update, so put exe there
-RUN go mod init cli_test && go mod tidy && go build -o bin/run
-`
-
-const testMain = `package main
-
-import (
-	"bytes"
-	"context"
-	"encoding/xml"
-	"log"
-	"net"
-	"net/http"
-	"os"
-	"os/exec"
-	"strings"
-	"sync"
-	"time"
-)
-
-func main() {
-	if os.Args[1] == "update_files" {
-		return
-	}
-	var wg sync.WaitGroup
-
-	// print the line that fails
-	log.SetFlags(log.Lshortfile)
-
-	go connectivityCheck(&wg)
-	checkIfRoot()
-	proxyCheck()
-
-	wg.Wait()
-}
-
-func checkIfRoot() {
-	buf := &bytes.Buffer{}
-	cmd := exec.Command("id", "-u")
-	cmd.Stdout = buf
-	if err := cmd.Run(); err != nil {
-		log.Fatalln(err)
-	}
-	userID := strings.TrimSpace(buf.String())
-	if userID == "0" {
-		log.Fatalln("User is root")
-	}
-}
-
-func connectivityCheck(wg *sync.WaitGroup) {
-	wg.Add(1)
-
-	var d net.Dialer
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	_, err := d.DialContext(ctx, "tcp", "1.1.1.1:22")
-	if err != nil && err.Error() != "dial tcp 1.1.1.1:22: i/o timeout" {
-		log.Fatalln(err)
-	}
-	if err == nil {
-		log.Fatalln("a connection shouldn't be possible")
-	}
-
-	wg.Done()
-}
-
-func proxyCheck() {
-	res, err := http.Get("https://example.com")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer res.Body.Close()
-	var v any
-	if err = xml.NewDecoder(res.Body).Decode(&v); err != nil {
-		log.Fatalln(err)
-	}
-}
-`
