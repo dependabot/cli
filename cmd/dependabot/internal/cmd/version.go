2f3fdc69ebfd5305cb23fccce458bbad7399431e
Jake Coffman <jake@jakecoffman.com>
2025-02-18 08:00:44 -0600

    use new build version feature

--- cmd/dependabot/internal/cmd/version.go
+++ cmd/dependabot/internal/cmd/version.go
@@ -1,42 +1,24 @@
 package cmd
 
 import (
-	"fmt"
-	"regexp"
+	"log"
 	"runtime/debug"
 )
 
 // ldflags inserts the version here on release
 var version string
 
-var timestampRegex = regexp.MustCompile("[^a-zA-Z0-9]+")
-
 func Version() string {
 	if version == "" {
 		version = "0.0.0-dev"
-		commit := ""
-		timestamp := ""
-		modified := false
 
-		info, _ := debug.ReadBuildInfo()
-		for _, entry := range info.Settings {
-			if entry.Key == "vcs.revision" && len(entry.Value) >= 7 {
-				commit = entry.Value[:7] // short ref
-			}
-
-			if entry.Key == "vcs.modified" {
-				modified = entry.Value == "true"
-			}
-
-			if entry.Key == "vcs.time" {
-				timestamp = timestampRegex.ReplaceAllString(entry.Value, "")
-			}
+		info, ok := debug.ReadBuildInfo()
+		if !ok {
+			log.Println("debug.ReadBuildInfo failed")
+			return version
 		}
-
-		if modified && timestamp != "" {
-			return fmt.Sprintf("%s+%s", version, timestamp)
-		} else if commit != "" {
-			return fmt.Sprintf("%s+%s", version, commit)
+		if info.Main.Version != "" {
+			version = info.Main.Version
 		}
 	}
 
