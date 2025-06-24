dependabot/cli

commit 5c2977bfb67c80109afa7dcd40f4617f1bfcfb7c

fix CLI removing binary content from stdout (#465)
* fix CLI removing binary content from stdout

* remove advanced debugging techniques

internal/server/api.go
@@ -121,6 +121,9 @@ func (a *API) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	parts := strings.Split(r.URL.String(), "/")
 	kind := parts[len(parts)-1]
+
+	a.outputRequestData(kind, data)
+
 	actual, err := decodeWrapper(kind, data)
 	if err != nil {
 		a.pushError(err)
@@ -134,7 +137,6 @@ func (a *API) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	if kind == "increment_metric" {
 		// Let's just output the metrics data and stop
-		a.outputRequestData(kind, actual)
 		return
 	}
 
@@ -144,7 +146,6 @@ func (a *API) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if !a.hasExpectations {
-		a.outputRequestData(kind, actual)
 		return
 	}
 
@@ -178,12 +179,12 @@ func (a *API) assertExpectation(kind string, actual *model.UpdateWrapper) {
 	}
 }
 
-func (a *API) outputRequestData(kind string, actual *model.UpdateWrapper) {
+func (a *API) outputRequestData(kind string, data []byte) {
 	if a.writer != nil {
 		// output the data received to stdout
 		if err := json.NewEncoder(a.writer).Encode(map[string]any{
 			"type": kind,
-			"data": actual.Data,
+			"data": string(data),
 		}); err != nil {
 			// Fail so the user knows stdout is not working
 			log.Panicln("Failed to write to stdout: ", err)

internal/server/api_test.go
@@ -1,8 +1,16 @@
 package server
 
 import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"github.com/dependabot/cli/internal/model"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -27,4 +35,64 @@ func TestAPI_ServeHTTP(t *testing.T) {
 			t.Errorf("expected status code %d, got %d", http.StatusNotImplemented, response.Code)
 		}
 	})
+}
+
+func TestAPI_CreatePullRequest_ReplacesBinaryWithHash(t *testing.T) {
+	var stdout bytes.Buffer
+
+	api := NewAPI(nil, &stdout)
+	defer api.Stop()
+
+	content := base64.StdEncoding.EncodeToString([]byte("Hello, world!"))
+	hash := sha256.Sum256([]byte(content))
+	expectedHashedContent := hex.EncodeToString(hash[:])
+
+	// Construct the request body for create_pull_request
+	createPullRequest := model.CreatePullRequest{
+		UpdatedDependencyFiles: []model.DependencyFile{
+			{
+				Content:         content,
+				ContentEncoding: "base64",
+			},
+		},
+	}
+	var body bytes.Buffer
+	if err := json.NewEncoder(&body).Encode(model.UpdateWrapper{Data: createPullRequest}); err != nil {
+		t.Fatalf("failed to encode request body: %v", err)
+	}
+
+	url := "http://127.0.0.1:" + // use the API's port
+		fmt.Sprintf("%d/create_pull_request", api.Port())
+	req, err := http.NewRequest("POST", url, &body)
+	if err != nil {
+		t.Fatalf("failed to create request: %v", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatalf("failed to send request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if len(api.Errors) > 0 {
+		t.Fatalf("expected no errors, got %d errors: %v", len(api.Errors), api.Errors)
+	}
+
+	// The API should have replaced the content with a SHA hash in a.Actual.Output
+	if len(api.Actual.Output) != 1 {
+		t.Fatalf("expected 1 output, got %d", len(api.Actual.Output))
+	}
+	if api.Actual.Output[0].Type != "create_pull_request" {
+		t.Fatalf("expected output type 'create_pull_request', got '%s'", api.Actual.Output[0].Type)
+	}
+	if api.Actual.Output[0].Expect.Data.(model.CreatePullRequest).UpdatedDependencyFiles[0].Content != expectedHashedContent {
+		t.Errorf("expected content to be 'hello', got '%s'", api.Actual.Output[0].Expect.Data.(model.CreatePullRequest).UpdatedDependencyFiles[0].Content)
+	}
+
+	// stdout should contain the original content so folks can create PRs
+	if !strings.Contains(stdout.String(), content) {
+		t.Errorf("expected stdout to contain the original content, got '%s'", stdout.String())
+	}
 }

commit 712c619c889938114d5e14233f9d8c85d825f738

fix {"data":{"data" caused by #465

internal/server/api.go
@@ -122,11 +122,19 @@ func (a *API) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	parts := strings.Split(r.URL.String(), "/")
 	kind := parts[len(parts)-1]
 
-	a.outputRequestData(kind, data)
-
 	actual, err := decodeWrapper(kind, data)
 	if err != nil {
 		a.pushError(err)
+	}
+
+	a.outputRequestData(kind, actual)
+
+	if kind == "create_pull_request" && actual != nil {
+		createPR := actual.Data.(model.CreatePullRequest)
+		createPR.UpdatedDependencyFiles = replaceBinaryWithHash(createPR.UpdatedDependencyFiles)
+	} else if kind == "update_pull_request" && actual != nil {
+		updatePR := actual.Data.(model.UpdatePullRequest)
+		updatePR.UpdatedDependencyFiles = replaceBinaryWithHash(updatePR.UpdatedDependencyFiles)
 	}
 
 	if actual == nil {
@@ -179,12 +187,12 @@ func (a *API) assertExpectation(kind string, actual *model.UpdateWrapper) {
 	}
 }
 
-func (a *API) outputRequestData(kind string, data []byte) {
+func (a *API) outputRequestData(kind string, data *model.UpdateWrapper) {
 	if a.writer != nil {
 		// output the data received to stdout
 		if err := json.NewEncoder(a.writer).Encode(map[string]any{
 			"type": kind,
-			"data": string(data),
+			"data": data.Data,
 		}); err != nil {
 			// Fail so the user knows stdout is not working
 			log.Panicln("Failed to write to stdout: ", err)
@@ -221,15 +229,9 @@ func decodeWrapper(kind string, data []byte) (actual *model.UpdateWrapper, err e
 	case "update_dependency_list":
 		actual.Data, err = decode[model.UpdateDependencyList](data)
 	case "create_pull_request":
-		var createPR model.CreatePullRequest
-		createPR, err = decode[model.CreatePullRequest](data)
-		createPR.UpdatedDependencyFiles = replaceBinaryWithHash(createPR.UpdatedDependencyFiles)
-		actual.Data = createPR
+		actual.Data, err = decode[model.CreatePullRequest](data)
 	case "update_pull_request":
-		var updatePR model.UpdatePullRequest
-		updatePR, err = decode[model.UpdatePullRequest](data)
-		updatePR.UpdatedDependencyFiles = replaceBinaryWithHash(updatePR.UpdatedDependencyFiles)
-		actual.Data = updatePR
+		actual.Data, err = decode[model.UpdatePullRequest](data)
 	case "close_pull_request":
 		actual.Data, err = decode[model.ClosePullRequest](data)
 	case "mark_as_processed":

internal/server/api_test.go
@@ -10,7 +10,6 @@ import (
 	"github.com/dependabot/cli/internal/model"
 	"net/http"
 	"net/http/httptest"
-	"strings"
 	"testing"
 )
 
@@ -35,6 +34,10 @@ func TestAPI_ServeHTTP(t *testing.T) {
 			t.Errorf("expected status code %d, got %d", http.StatusNotImplemented, response.Code)
 		}
 	})
+}
+
+type Wrapper[T any] struct {
+	Data T `json:"data"`
 }
 
 func TestAPI_CreatePullRequest_ReplacesBinaryWithHash(t *testing.T) {
@@ -92,7 +95,11 @@ func TestAPI_CreatePullRequest_ReplacesBinaryWithHash(t *testing.T) {
 	}
 
 	// stdout should contain the original content so folks can create PRs
-	if !strings.Contains(stdout.String(), content) {
+	var wrapper Wrapper[model.CreatePullRequest]
+	if err := json.NewDecoder(&stdout).Decode(&wrapper); err != nil {
+		t.Fatalf("failed to decode stdout: %v", err)
+	}
+	if wrapper.Data.UpdatedDependencyFiles[0].Content != content {
 		t.Errorf("expected stdout to contain the original content, got '%s'", stdout.String())
 	}
 }